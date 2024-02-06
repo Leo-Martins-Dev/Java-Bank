14d42a1b3847d8bd4e494393bf2e32f7272b40f4
Leonardo Martins <leo@MacBook-Air-de-Leonardo.local>
2024-02-06 10:52:55 -0300

    refactor: update class options for better encapsulation and readability

--- src/main/java/org/javabank/Main.java
+++ src/main/java/org/javabank/Main.java
@@ -5,31 +5,7 @@ import org.javabank.userinterface.Options;
 public class Main {
     public static void main(String[] args) {
         Options options = new Options();
-        int numberOption;
-        do {
-            options.showOptions();
-            numberOption = options.scannerOptions();
-            try {
-                switch (numberOption) {
-                    case 1:
-                        //options.deposit();
-                        break;
-                    case 2:
-                        //options.cashOut();
-                        break;
-                    case 3:
-                        //options.showBalance();
-                        break;
-                    case 4:
-                        System.out.println("Exiting...");
-                        break;
-                    default:
-                        System.out.println("Invalid option. Please try again.");
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println("An error occurred: " + e.getMessage());
-            }
-        } while (numberOption != 4);
+
+        options.interfaceLoop();
     }
 }
--- src/main/java/org/javabank/userinterface/Options.java
+++ src/main/java/org/javabank/userinterface/Options.java
@@ -3,9 +3,13 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Options {
-    Scanner scanner = new Scanner(System.in);
 
-    public void showOptions(){
+    private static final int DEPOSIT = 1;
+    private static final int CASH_OUT = 2;
+    private static final int SHOW_BALANCE = 3;
+    private static final int QUIT = 4;
+
+    private void showOptions() {
         System.out.println("Please choose one of the options");
         System.out.println("1. Deposit");
         System.out.println("2. Cash out");
@@ -13,13 +17,46 @@ public class Options {
         System.out.println("4. Quit");
     }
 
-    public int scannerOptions(){
+    public void interfaceLoop() {
+        int numberOption;
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
+                showOptions();
+                numberOption = scannerOptions(scanner);
+                try {
+                    switch (numberOption) {
+                        case DEPOSIT:
+                            //method_Deposit
+                            break;
+                        case CASH_OUT:
+                            //method_Cash_Out
+                            break;
+                        case SHOW_BALANCE:
+                            //method_Balance
+                            break;
+                        case QUIT:
+                            System.out.println("Exiting...");
+                            break;
+                        default:
+                            System.out.println("Invalid option. Please try again.");
+                            break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("An error occurred: " + e.getMessage());
+                }
+            } while (numberOption != 4);
+        }
+    }
+
+    private int scannerOptions(Scanner scanner) {
         try {
             return scanner.nextInt();
         } catch (InputMismatchException e) {
             System.out.println("Please insert a number");
+            showOptions();
             scanner.next();
-            return scannerOptions();
+            return scannerOptions(scanner);
         }
     }
 }
+
